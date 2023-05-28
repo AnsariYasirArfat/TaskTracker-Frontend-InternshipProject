faf2e8c7d8edfe0347a0d322a37625b0e61bf55a
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-05-28 21:51:41 +0530

    Router installed & loginPage imported & login/out function added

--- Projects/02. Todo App_React & Bootstrap/todos-app/src/App.js
+++ Projects/02. Todo App_React & Bootstrap/todos-app/src/App.js
@@ -1,13 +1,43 @@
-import Header from "./Mycomponents/Header";
-import Footer from "./Mycomponents/Footer";
-import Todos from "./Mycomponents/Todos";
-import AddTodo from "./Mycomponents/AddTodo";
 import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./CSS/App.css";
 
+import Header from "./Mycomponents/Header";
+import Footer from "./Mycomponents/Footer";
+import Todos from "./Mycomponents/Todos";
+import AddTodo from "./Mycomponents/AddTodo";
+import LoginPage from "./Mycomponents/LoginPage";
+
 function App() {
+  // For Login & Signup
+  const [loggedIn, setLoggedIn] = useState(false);
+  const handleLogin = (token) => {
+    // Handle the successful login and set the loggedIn state to true
+    localStorage.setItem("token", token);
+    setLoggedIn(true);
+  };
+  useEffect(() => {
+    // Check if the user is already logged in
+    const token = localStorage.getItem("token");
+    if (token) {
+      setLoggedIn(true);
+    }
+  }, []);
+
+  // For Logout button
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setLoggedIn(false);
+  };
+
+  // Getting the data from localStorage
   let initTodo;
   if (localStorage.getItem("todos") === null) {
     initTodo = [];
@@ -92,23 +122,44 @@ function App() {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
   return (
-    <>
-      <Header
-        title="TaskTracker"
-        filterText={filterText}
-        onFilterTextChange={setFilterText}
-      />
-      <AddTodo addTodo={addTodo} />
-      <Todos
-        todos={todos}
-        onDelete={onDelete}
-        onEdit={onEdit}
-        onChecked={onChecked}
-        filterText={filterText}
-      />
-      <ToastContainer autoClose={1000} />
-      <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            loggedIn ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+          }
+        />
+        <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+        <Route
+          path="/dashboard"
+          element={
+            loggedIn ? (
+              <>
+                <Header
+                  title="TaskTracker"
+                  filterText={filterText}
+                  onFilterTextChange={setFilterText}
+                />
+                <AddTodo addTodo={addTodo} />
+                <button onClick={handleLogout}>Logout</button>
+                <Todos
+                  todos={todos}
+                  onDelete={onDelete}
+                  onEdit={onEdit}
+                  onChecked={onChecked}
+                  filterText={filterText}
+                />
+                <ToastContainer autoClose={1000} />
+                <Footer />
+              </>
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
